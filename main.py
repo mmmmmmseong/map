9bdf1686efc62f69138e1b8fc76415893872151a
강민성 <nomatmu@gmail.com>
2026-05-08 15:47:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from streamlit_folium import st_folium
 import pandas as pd
 import map
 
-st.set_page_config(layout="wide")
+st.set_page_config(page_title="남동고 등산 메이트", layout="wide")
 
 m = map.map()
 
-st.title("Highschool Map")
-st.text("Incheon - Namdong")
+st.title("2026 학교 등산 행사 지도")
+#st.text("Incheon - Namdong")
 st_folium(m, use_container_width=True, height=500)
 
