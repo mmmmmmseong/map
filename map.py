9ce611210ac3c078103047637ca94f60ffeaf298
mmmmmmseong <nomatmu@gmail.com>
2026-06-05 15:52:39 +0900

    등산로 지점 표시 수정

--- map.py
+++ map.py
@@ -10,7 +10,7 @@ def map(tiles="Cartodb Positron", show_schools=True, show_paths=True):
     
     m = folium.Map(
         location=[37.406046, 126.721473],
-        zoom_start=14,
+        zoom_start=12,
         tiles=tiles
     )
     
