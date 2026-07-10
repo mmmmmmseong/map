ffde672057168769b26ab2fb55508e867a9b446c
mmmmmmseong <nomatmu@gmail.com>
2026-07-10 16:09:48 +0900

    등산로 표시

--- map.py
+++ map.py
@@ -16,7 +16,7 @@ def map(tiles="Cartodb Positron", show_schools=True, show_paths=True, show_pline
     m = folium.Map(
         location=[37.406046, 126.721473],
         zoom_start=17,
-        tiles=tiles,
+        tiles=tiles
     )
 
     m.save("index.html")
