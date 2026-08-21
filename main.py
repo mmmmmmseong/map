6f688594036001282df4bef9158708a349e4bae6
mmmmmmseong <nomatmu@gmail.com>
2026-08-21 14:22:35 +0900

    등산로 사진

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="남동고 등산 메이트", layout="wide")
 
 pages = [
-    st.Page("map_page.py", title="지도", icon="🗺️", default="True"),
+    st.Page("map_page.py", title="지도", icon="🗺️", default=True),
     st.Page("info_page.py", title="산행 안내", icon="ℹ️"),
 ]
 
